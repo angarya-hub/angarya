05ed29c70bb8d1cdf3be4c3ddbfbd2d6324f74f5
andapar <160366488+andapar@users.noreply.github.com>
2024-09-13 13:26:38 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
-          href="https://liman.involve.me/market-research-survey-for-finance"
+          href="https://hcwpbcxqu4u.typeform.com/to/FJfpKSRF"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
