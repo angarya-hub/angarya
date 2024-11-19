66eee89b0c476c53491d985d17b5f9c876b5e57e
andapar <160366488+andapar@users.noreply.github.com>
2024-11-19 11:17:12 +0300

    Update page.tsx

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -17,14 +17,13 @@ const PrivacyPolicyandAcikRizaBeyani: FC = () => {
         pazarlama ve tanıtım aktivitelerini tasarlamak, tarafıma sunulacak uygun ürün ve hizmetlerin belirlenmesine ve geliştirilmesine yönelik kredi, davranış
         skorlama, pazar araştırması, pazarlama amacıyla veri analitiği çalışmaları yapmak, müşteri memnuniyeti ve tecrübesine yönelik olarak çalışmalar yapmak
         amaçlarıyla 6698 sayılı Kişisel Verilerin Korunması Kanunu ve diğer ilgili mevzuata uygun olarak veri sorumlusu sıfatıyla Liman tarafından işlenmesine,
-        özel nitelikli kişisel verilerden olan yüz tanıma (Face ID) verisi haricinde kalan genel nitelikli kişisel verilerimin kişisel verilerimin sadece liman'da s
-        saklanılacağına ve 3.parti veya herhangi bir şirket ile paylaşılmayacağını taahhüt ederiz.
-      
+        özel nitelikli kişisel verilerden olan yüz tanıma (Face ID) verisi haricinde kalan genel nitelikli kişisel verilerimin kişisel verilerimin sadece Liman'da saklanılacağına
+        ve 3. parti veya herhangi bir şirket ile paylaşılmayacağını taahhüt ederiz.
+      </p>
+
       <p>In accordance with the Personal Data Protection Law No. 6698, I am hereby informed about how my personal data will be processed by Yunus Cebeci 
-        (“Liman”), the purposes of processing, to whom and for what purposes my personal data may be transferred, the method and legal basis of 
-        data collection, and my rights as stated in Article 11 of the Law, through the “USER INFORMATION TEXT ON THE PROCESSING OF PERSONAL DATA” provided to me.
-        In this context, my personal data, including identity, contact, digital trace, financial, marketing, and customer transaction data, and sensitive personal 
-        data, such as facial recognition data, will be processed by Liman for the purposes of carrying out activities to ensure the best use of products and services
+        (“Liman”), the purposes of processing, to whom and for what purposes my personal data may be transferred, the method and legal basis of data collection, and my rights as stated in Article 11 of the Law, through the “USER INFORMATION TEXT ON THE PROCESSING OF PERSONAL DATA” provided to me.
+        In this context, my personal data, including identity, contact, digital trace, financial, marketing, and customer transaction data, and sensitive personal data, such as facial recognition data, will be processed by Liman for the purposes of carrying out activities to ensure the best use of products and services
         offered by Liman, conducting identity verification through video calls, performing biometric comparison of my facial image and identity document, marketing 
         the products and services, planning and conducting marketing, promotion, and communication activities, offering proposals through all channels including digital
         platforms and social media, conducting promotions, cross-selling, and campaign activities, and designing personalized marketing and promotional activities in 
@@ -32,30 +31,31 @@ const PrivacyPolicyandAcikRizaBeyani: FC = () => {
         Additionally, my personal data may be used for purposes such as credit scoring, behavioral scoring, market research, data analytics for marketing purposes, 
         and customer satisfaction studies, in order to determine and develop suitable products and services. All of this processing will be carried out in accordance 
         with the Personal Data Protection Law No. 6698 and other relevant legislation, and Liman will process my personal data as the data controller, except for the 
-        sensitive personal data related to facial recognition (Face ID).We guarantee that your data will be stored by Liman and will not be shared with any third party or company
-      
-        I acknowledge, confirm, and undertake that I have given my explicit consent for the processing and transfer of my personal data to the aforementioned parties without 
-        any influence, and that I have been informed that I can withdraw my consent at any time.</p>
+        sensitive personal data related to facial recognition (Face ID). We guarantee that your data will be stored by Liman and will not be shared with any third party or company.
+      </p>
 
-      <h2>Yunus Cebeci (“Liman”) PAZARLAMA & ELEKTRONİK İLETİ İZNİ -MARKETING & ELECTRONIC COMMUNICATION CONSENT< </h2>
+      <h2>Yunus Cebeci (“Liman”) PAZARLAMA & ELEKTRONİK İLETİ İZNİ - MARKETING & ELECTRONIC COMMUNICATION CONSENT</h2>
 
-      <p>Vermiş olduğunuz izin aşağıda &quot; Elektronik İletişim İzni Alınan Şahıslar&quot; başlığı altında belirtilen tüm şahısların her biri adına elektronik
-        ileti gönderilmesini (Elektronik İletişim İzni) ve aşağıda &quot;Veri İşleme İzni Alınan Şirketler&quot; başlığı altında belirtilen şirketler adına kişisel
+      <p>Vermiş olduğunuz izin aşağıda "Elektronik İletişim İzni Alınan Şahıslar" başlığı altında belirtilen tüm şahısların her biri adına elektronik
+        ileti gönderilmesini (Elektronik İletişim İzni) ve aşağıda "Veri İşleme İzni Alınan Şirketler" başlığı altında belirtilen şirketler adına kişisel
         verilerinizin işlenip paylaşılmasını (Veri İşleme İzni) kapsamaktadır. İletişim bilgileriniz dışındaki kişisel verilerinizin beyan formunda belirtilen
         amaçlarla kullanılmasını istemiyorsanız, info@liman.finance üzerinden bize mail ile bilgilerimin silinmesini talep ediyorum mesajı gönderebilirsiniz bu koşulda
-        uygulamaya giriş yaptığınız kullanıcı hesabının bütün bilgilerinin silineceğini taahhüt ederiz.</p>
+        uygulamaya giriş yaptığınız kullanıcı hesabının bütün bilgilerinin silineceğini taahhüt ederiz.
+      </p>
+      
       <p>The consent you have given covers the sending of electronic communications (Electronic Communication Consent) on behalf of all the companies
-        listed under the title “ Authorized for Electronic Communication” below, and the processing and sharing of your personal data on behalf of the
-        companies listed under the title “ Authorized for Data Processing” below.You can send an email requesting the deletion of your information to info@liman.finance.
-        In this case, we guarantee that all the information associated with the user account you logged into the application will be deleted</p>
+        listed under the title “Authorized for Electronic Communication” below, and the processing and sharing of your personal data on behalf of the
+        companies listed under the title “Authorized for Data Processing” below. You can send an email requesting the deletion of your information to info@liman.finance.
+        In this case, we guarantee that all the information associated with the user account you logged into the application will be deleted.
+      </p>
 
       <h3>Veri İşleme İzni Alınan Şahıslar/ Individuals Authorized for Data Processing</h3>
       <p>Yunus Cebeci</p>
 
-      <p>Kişisel verilerimin Yunus Cebeci tarafından yukarıdaki amaçlar doğrultusunda işlenmesini ve  elektronik ileti gönderilmesini kabul ediyorum.</p>
-      <p>I accept that my personal data will be processed by Yunus Cebeci for the purposes stated above and that electronic communication will be sent.<p>
+      <p>Kişisel verilerimin Yunus Cebeci tarafından yukarıdaki amaçlar doğrultusunda işlenmesini ve elektronik ileti gönderilmesini kabul ediyorum.</p>
+      <p>I accept that my personal data will be processed by Yunus Cebeci for the purposes stated above and that electronic communication will be sent.</p>
     </div>
   );
 };
 
-export default AcikRizaBeyani;
+export default PrivacyPolicyandAcikRizaBeyani;
