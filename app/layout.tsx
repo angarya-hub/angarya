b6b1c97116ab483b51cc01dc55caf3374b9eafdb
angarya-hub <angarya.online@gmail.com>
2024-11-22 15:31:27 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Profesyonel Sanal Asistanınız",
+  title: "Sanal Asistanınız",
   description: "Online Angarya İşleriniz Bize Emanet",
 };
 
